33c42226707e2d459252b461af7bedad70024c61
Gaurav Nikam <gauravpn.a2@gmail.com>
2026-01-13 22:34:07 +0530

    Added glad

--- src/Triangle/Triangle.cpp
+++ src/Triangle/Triangle.cpp
@@ -1,5 +1,6 @@
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 void DrawTriangle() {
   glfwInit();
@@ -7,10 +8,15 @@ void DrawTriangle() {
   GLFWwindow *window;
 
   window = glfwCreateWindow(800, 800, "DrawTriangle", NULL, NULL);
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return;
+  }
   glfwMakeContextCurrent(window);
 
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
+
     glfwSwapBuffers(window);
   }
 }
