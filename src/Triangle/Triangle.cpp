d8addb34161ab18dedeb38508da6ab09d6a8f1bf
Gaurav Nikam <gauravpn.a2@gmail.com>
2026-01-14 23:45:32 +0530

    Fixed glad issue

--- src/Triangle/Triangle.cpp
+++ src/Triangle/Triangle.cpp
@@ -10,14 +10,15 @@ void DrawTriangle() {
   GLFWwindow *window;
 
   window = glfwCreateWindow(800, 800, "DrawTriangle", NULL, NULL);
+
+  glfwMakeContextCurrent(window);
+
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     std::cout << "Failed to initialize GLAD" << std::endl;
     return;
   }
   glViewport(0, 0, 800, 800);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-  glfwMakeContextCurrent(window);
 
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
