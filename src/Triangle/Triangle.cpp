c4160208924b3cfb3bccc52befe14f44a5f20a3a
Gaurav Nikam <gauravpn.a2@gmail.com>
2026-01-15 22:21:41 +0530

    Added keyboard access

--- src/Triangle/Triangle.cpp
+++ src/Triangle/Triangle.cpp
@@ -3,6 +3,14 @@
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+  else if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS) {
+    glClearColor(0.2, 1, 0.6, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+  }
+}
 
 void DrawTriangle() {
   glfwInit();
@@ -22,6 +30,7 @@ void DrawTriangle() {
 
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
+    processInput(window);
     glfwSwapBuffers(window);
   }
   glfwTerminate();
