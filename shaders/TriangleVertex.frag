20f7d55f6231a4d11e815acd2d64c3c7eac373ef
Gaurav Nikam <gauravpn.a2@gmail.com>
2026-01-23 23:45:51 +0530

    Trinagle complete

--- shaders/TriangleVertex.frag
+++ shaders/TriangleVertex.frag
@@ -1,7 +1,6 @@
 #version 330 core
-
 out vec4 FragColor;
-
-void main(){
-	FragColor = vec4(1.f,0.5f , 0.2f , 1.0f);	
+void main()
+{
+	FragColor = vec4(1.f,0.f , 0.f , 1.0f);	
 }
--- src/Triangle/Triangle.cpp
+++ src/Triangle/Triangle.cpp
@@ -1,5 +1,5 @@
 #include "Triangle.hpp"
-#include <string>
+#include <filesystem>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window) {
@@ -42,7 +42,7 @@ GLuint LoadShader(const char *vertPath, const char *fragPath) {
   }
 
   // --- Shader Program ---
-  GLuint shaderProgram = glCreateProgram();
+  unsigned int shaderProgram = glCreateProgram();
   glAttachShader(shaderProgram, vertexShader);
   glAttachShader(shaderProgram, fragmentShader);
   glLinkProgram(shaderProgram);
@@ -61,8 +61,14 @@ GLuint LoadShader(const char *vertPath, const char *fragPath) {
 }
 
 void DrawTriangle() {
-  glfwInit();
+  if (!glfwInit()) {
+    std::cout << "failed to open window";
+  }
 
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
   GLFWwindow *window;
 
@@ -74,27 +80,38 @@ void DrawTriangle() {
     std::cout << "Failed to initialize GLAD" << std::endl;
     return;
   }
+  GLuint shaderProgram = LoadShader("../shaders/TriangleVertex.vert",
+                                    "../shaders/TriangleVertex.frag");
   unsigned int VBO;
+  unsigned int VAO;
+  glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
+  glBindVertexArray(VAO);
 
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  glViewport(0, 0, 800, 800);
-  GLuint shaderProgram = LoadShader("../../shaders/TriangleVertex.vert",
-                                    "../../shaders/TriangleVertex.frag");
-
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
+  glBindVertexArray(0);
 
-  glUseProgram(shaderProgram);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
     processInput(window);
+    glClearColor(0.2f, 0.3f, 0.3f, 1.f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
     glfwSwapBuffers(window);
   }
+
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteProgram(shaderProgram);
   glfwTerminate();
 }
 
