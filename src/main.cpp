d81b059140372c86b672f700056e315a455e55d2
Gaurav Nikam <gauravpn.a2@gmail.com>
2026-02-19 23:15:49 +0530

    Code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,5 @@
 #include "MandelBrot/MandelBrot.hpp"
 #include "Shapes/Shapes.hpp"
 #include "Triangle/Triangle.hpp"
-#include "glm/ext/matrix_transform.hpp"
-#include "glm/fwd.hpp"
 
 int main() { DrawSqaure(); }
