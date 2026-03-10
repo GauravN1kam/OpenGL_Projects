6441e0547ffc773f40d73e396bba54233c976cd9
Gaurav Nikam <gauravpn.a2@gmail.com>
2026-03-10 22:28:01 +0530

    Added global veriables

--- src/HelloWindow/HelloWindow.cpp
+++ src/HelloWindow/HelloWindow.cpp
@@ -1,10 +1,11 @@
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
+#define WIDTH 800
 
 void CallMain() {
   glfwInit();
   GLFWwindow *window;
-  window = glfwCreateWindow(800, 800, "First window", NULL, NULL);
+  window = glfwCreateWindow(WIDTH, 800, "First window", NULL, NULL);
 
   glfwMakeContextCurrent(window);
   while (!glfwWindowShouldClose(window)) {
