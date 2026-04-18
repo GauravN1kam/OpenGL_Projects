a9b7d907e577fd74f2a50f4127a13ee1f1893fb3
Gaurav Nikam <gauravpn.a2@gmail.com>
2026-04-19 00:14:42 +0530

    Cleanup

--- src/Going3D/Cube.cpp
+++ src/Going3D/Cube.cpp
@@ -11,45 +11,6 @@
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-// Vertex Shader
-const char *vertexShaderSource = R"(
-#version 330 core
-layout (location = 0) in vec3 aPos;
-
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
-out vec4 pos;
-
-void main() {
-		pos = projection * view * model * vec4(aPos, 1.0);
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
-}
-)";
-
-// Fragment Shader
-const char *fragmentShaderSource = R"(
-#version 330 core
-out vec4 FragColor;
-in vec4 pos;
-
-vec3 palette(float t)
-{
-
-	vec3 a = vec3(0.5,0.5,0.5);
-	vec3 b = vec3(0.5,0.5,0.5);
-	vec3 c = vec3(1.0,1.0,1.0);
-	vec3 d = vec3(0.263, 0.146,0.557);
-
-	return a + b*cos(6.28318*(c*t+d));
-}
-
-void main() {
-
-    FragColor = vec4(1.f,pos.x,pos.y, pos.z);
-}
-)";
-
 // Cube vertices (36 vertices)
 float vertices[] = {
     -0.5f, -0.5f, -0.5f, 0.5f,  -0.5f, -0.5f, 0.5f,  0.5f,  -0.5f,
@@ -98,23 +59,6 @@ void DrawCube() {
 
   glEnable(GL_DEPTH_TEST);
 
-  // Build shaders
-  // unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-  // glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-  // glCompileShader(vertexShader);
-  //
-  // unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-  // glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-  // glCompileShader(fragmentShader);
-  //
-  // unsigned int shaderProgram = glCreateProgram();
-  // glAttachShader(shaderProgram, vertexShader);
-  // glAttachShader(shaderProgram, fragmentShader);
-  // glLinkProgram(shaderProgram);
-  //
-  // glDeleteShader(vertexShader);
-  // glDeleteShader(fragmentShader);
-  //
   // VAO & VBO
   unsigned int VAO, VBO;
   glGenVertexArrays(1, &VAO);
@@ -137,11 +81,11 @@ void DrawCube() {
     glClearColor(0.1f, 0.1f, 0.15f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // glUseProgram(shaderProgram);
+    float time = glfwGetTime();
 
     // Transformations
     glm::mat4 model =
-        glm::rotate(glm::mat4(1.0f), 3.1415f, glm::vec3(1.f, 1.f, 0.0f));
+        glm::rotate(glm::mat4(1.0f), time, glm::vec3(0.f, 1.f, 0.0f));
 
     glm::mat4 view =
         glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
