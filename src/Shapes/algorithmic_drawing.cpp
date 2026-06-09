008065dfc6193f35826ae3c27f1f33df303719bb
Gaurav Nikam <gauravpn.a2@gmail.com>
2026-06-09 21:53:51 +0530

    started using shader

--- src/Shapes/algorithmic_drawing.cpp
+++ src/Shapes/algorithmic_drawing.cpp
@@ -33,6 +33,8 @@ void DrawLine() {
     glClearColor(0.1f, 0.1f, 0.15f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    shader.use();
+
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
